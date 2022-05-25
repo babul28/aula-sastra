3e90116a7ba68ed05d2a66b359c1f9e0ab470cb2
Miftahul Lubab <miftahul.lubab20@gmail.com>
2022-05-25 22:53:08 +0700

    WIP

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,16 +1,14 @@
 <x-post-layout title="Tentang">
     <p class="text-gray-700 mb-4 leading-loose">
         <strong class="text-2xl">Aula Sastra</strong> merupakan sebuah laman daring yang dikembangkan untuk
-        meningkatkan budaya
-        literasi generasi
-        muda Indonesia. Laman daring ini diharapkan dapat menjadi wadah tulisan atau karya generasi muda dan
-        keberadaannya dapat memotivasi generasi muda untuk terus menulis.
+        meningkatkan budaya literasi generasi muda Indonesia. Laman daring ini diharapkan dapat menjadi wadah tulisan
+        atau karya generasi muda dan keberadaannya dapat memotivasi generasi muda untuk terus menulis.
     </p>
 
     <p class="text-gray-700 mb-4 leading-loose">
         Selain itu, Aula Sastra juga diharapkan dapat bermanfaat bagi pendidik dan peserta didik sebagai sumber
         inspirasi maupun referensi. Aula sastra menyuguhkan berbagai karya yang dapat dinikmati, seperti puisi,
-        cerpen, dan naskah teater. Selain menyuguhkan karya, Aula Sastra juga memfasilitasi para generasi muda
+        cerpen, esai, dan naskah teater. Selain menyuguhkan karya, Aula Sastra juga memfasilitasi para generasi muda
         untuk bergabung ke dalam komunitas penulis, yaitu komunitas Aula Sastra.
     </p>
 
